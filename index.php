9219fa6d92d55d4570c34e82b4f618a3beab2d58
firmandpratama <100274493+firmandpratama@users.noreply.github.com>
2022-03-18 09:35:11 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CAFE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     
+    <div class="badan-utama">
+        
+        <header>
+            <h2>KOPI JANJI CINTA </h2>
+            <p>Indonesia Local BEANS locally roasted finest taste</p>
+        </header>
+ 
+        <nav class="navigasi">
+            <ul>
+                <li><a class="active" href="index.html">Home</a></li>
+                <li><a href="menu.html">Menu</a></li>
+				<li><a href="about/about.html">About</a><li>
+				<li><a href="Ela/contact.html">Contact Us</a><li>
+            </ul>
+        </nav>
+ 
+        <div class="banner">
+            <img src="gambar/calais.jpg" alt="cafekekinian">
+        </div>
+ 
+        <div class="menu-kiri">
+            <div class="kotak">
+                <h3>side bar</h3>
+                <p>
+                    Isi Form Pemesanan dengan Klik Tombol di bawah ini
+                </p>
+ 
+                <a class="tombol tombol-pesan" href="pesan.html">Pesan</a>
+            </div>
+        </div>
+ 
+        <div class="menu-tengah">
+            <div class="kotak">
+                <h3>BOBA MILK TEA</h3>
+                <img src="gambar/boba.jpg" alt="Minuman">
+ 
+                <p>Boba Milk Tea adalah minuman dengan bahan dasar teh, susu, dan boba.Boba milk tea yang 
+				klasik biasanya menggunakan teh hitam. Namun, seiring dengan perkembangan zaman dan inovasi,
+				di cafe ini biasanya menawarkan berbagai jenis teh untuk dipilih seperti melati, hijau, oolong, matcha,
+				earl grey, teh buah, dan sebagainya..............   
+                </p>
+ 
+                <a class="tombol tombol-lengkap" href="#">Selengkapnya</a>
+            </div>
+        </div>
+ 
+        <div class="menu-kanan">
+            <div class="kotak">
+                <h3>Jadwal Buka</h3>
+                <img src="gambar/daftar.jpg" alt="Minuman">
+                <h4 align="center">Jadwal Buka</h4>
+                <center>
+                    <p>Senin-Minggu 10.00 WIB - 23.59 WIB</p>
+                </center>
+            </div>
+		
+        <div class="kotak">
+		   <h3>Artike terkini</h3>
+		   
+		   <nav class="menu-artikel">
+		      <ul>
+			     <li><a href="https://www.alodokter.com/cara-meninggikan-badan-dengan-sehat-dan-alami">Cara Cepat Meninggikan Badan</a></li>
+				 <li><a href="https://doktersehat.com/contoh-mudah-menu-sehat-sehari/">Menu Makanan Sehat Kelurga</a></li>
+                 <li><a href="https://ilovelife.co.id/blog/cara-pencegahan-virus-corona-covid-19-menurut-anjuran/">Pencegahan Covid-19</a></li> 
+                 <li><a href="https://tirto.id/mitos-khasiat-daging-kambing-naikkan-tensi-gairah-seksual-cvJV">Khasiat Dari Sate Kambing</a></li>
+              </ul>
+           </nav>
+        </div>		   
+        </div>
+ 
+        <footer>
+              
+        </footer>
+ 
+    </div>
+ 
+</body>
+</html>
